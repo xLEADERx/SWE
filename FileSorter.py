3a6f802719d910e9b339ad61f59d78ed28f5b15b
xLEADERx <59559190+xLEADERx@users.noreply.github.com>
2020-01-06 16:50:26 +0530

    Update FileSorter.py

--- FileSorter.py
+++ FileSorter.py
@@ -7,17 +7,22 @@ from sys import platform as _platform
 
 
 def check_os():
+    global sep
     if _platform == "linux" or _platform == "linux2":
+        sep = '/'
         return 'ls'
     # linux
     elif _platform == "darwin":
+        sep = '/'
         return 'ls'
     # MAC OS X
     elif _platform == "win32":
-        return 'dir'
+        sep = '\\'
+        return 'dir /b'
     # Windows
     elif _platform == "win64":
-        return 'dir'
+        sep = '\\'
+        return 'dir /b'
 
 
 def sorter(path):
@@ -26,30 +31,30 @@ def sorter(path):
         files = sb.getoutput(check_os()).split('\n')
 
         for file in files:
-            if '.' in file and not os.path.isdir(path + '/' + file):
+            if '.' in file and not os.path.isdir(path + sep + file):
                 dir_name = file[file.rindex(".") + 1:].lower()
 
                 if not is_listed(path, dir_name):
 
-                    os.mkdir(path + '/' + dir_name)
-                    new_path = path + '/' + dir_name + '/' + file
-                    relocate(path + '/' + file, new_path)
+                    os.mkdir(path + sep + dir_name)
+                    new_path = path + sep + dir_name + sep + file
+                    relocate(path + sep + file, new_path)
 
                 else:
 
-                    new_path = path + '/' + dir_name + '/' + file
-                    relocate(path + '/' + file, new_path)
+                    new_path = path + sep + dir_name + sep + file
+                    relocate(path + sep + file, new_path)
 
             elif not is_dir(path, file):
 
                 if not is_listed(path, "etc_files"):
-                    os.mkdir(path + '/' + "etc_files")
+                    os.mkdir(path + sep + "etc_files")
 
                 if is_listed(path, "etc_files"):
-                    new_path = path + '/' + "etc_files" + '/' + file
-                    relocate(path + '/' + file, new_path)
+                    new_path = path + sep + "etc_files" + sep + file
+                    relocate(path + sep + file, new_path)
 
-        return "[+] Successfully sorted all files"
+        return "[+] Successfully Sorted all files"
 
     except Exception as e:
         return e
@@ -64,7 +69,7 @@ def is_listed(path, target):
 
 
 def is_dir(path, file):
-    if os.path.isdir(path + '/' + file):
+    if os.path.isdir(path + sep + file):
         return True
     else:
         return False
@@ -75,5 +80,6 @@ def relocate(path, new_path):
 
 
 if __name__ == "__main__":
+    sep = '/'
     dir_path = input("Input path: ")
     print(sorter(dir_path))
